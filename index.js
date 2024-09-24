f7215704cdce4ffacfad805afab3c1048e8a3e27
Debayan mukherjee <120113934+uxRakhal@users.noreply.github.com>
2024-09-24 16:07:30 +0530

    le

--- index.js
+++ index.js
@@ -39,3 +39,14 @@ lightbox.addEventListener('click', (e) => {
     lightbox.style.display = 'none';
   }
 });
+
+document.getElementById('menu').addEventListener('click', function() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  
+  if (mobileMenu.style.display === 'block') {
+      mobileMenu.style.display = 'none';
+  } else {
+      mobileMenu.style.display = 'block';
+  }
+});
+
